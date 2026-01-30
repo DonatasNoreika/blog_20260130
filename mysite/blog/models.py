fa5c61c0ea96fdc072bb290aaee06b408fb2e0ae
DonatasNoreika <donoras@gmail.com>
2026-01-30 09:13:14 +0200

    +Prie posto rodyti komentarų skaičių

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -10,6 +10,9 @@ class Post(models.Model):
                                null=True, blank=True)
     created = models.DateTimeField(auto_now_add=True)
 
+    def comments_count(self):
+        return self.comments.count()
+
     def __str__(self):
         return self.title
 
